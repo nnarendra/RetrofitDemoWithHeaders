f24cf32df54040e92902096dce19941b0c3e5cd8
Narendra Nadendla <nnarendra.info@gmail.com>
2018-02-09 20:35:29 +0530

    first commit

---
+++ app/src/main/java/com/naren/connectingwithserverviaretrofit/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.naren.connectingwithserverviaretrofit
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.naren.connectingwithserverviaretrofit.activityies.LoginActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+
+class SplashActivity : AppCompatActivity() {
+
+
+    // Splash screen timer
+    private val SPLASH_TIME_OUT:Long = 3000
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setSupportActionBar(toolbar)
+        Handler().postDelayed(Runnable
+        {
+
+            val i = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(i)
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+
+    }
+
+    override fun onBackPressed() {
+
+    }
+}
---
+++ app/src/main/java/com/naren/connectingwithserverviaretrofit/activityies/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.naren.connectingwithserverviaretrofit.activityies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.naren.connectingwithserverviaretrofit.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
